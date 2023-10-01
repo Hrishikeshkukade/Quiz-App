c8e889a82dcaff749926747299b8747cdd37a4e7
Hrishikesh Kukade <hrishikeshkukade6@gmail.com>
2023-10-01 23:23:19 +0530

    Added

---
+++ lint-staged.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    // Format JavaScript and JSON files with Prettier
+    '*.{js,json}': 'prettier --write',
+  
+    // Add other file types and corresponding format commands as needed
+  };
